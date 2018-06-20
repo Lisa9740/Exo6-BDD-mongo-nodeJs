d9b701d7585d90942696ca1d60e060194c29dbb7
Lisa9740 <barretalison@gmail.co>
2018-06-20 16:08:15 +0400

    ajout du formulaire pour insérer les donnée dans bdd

--- myapp/index.js
+++ myapp/index.js
@@ -2,7 +2,9 @@ var express = require('express')
 var app = express()
 var MongoClient = require("mongodb").MongoClient;
 var MongoObjectID = require("mongodb").ObjectID;
-
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -37,7 +39,38 @@ app.get('/', function (req, res) {
 });
 })
 
+// rajouter une entrée dans la base de donnée
+app.post('/addition', function(req, res) {
+    var name = req.body.name; 
+    var genre = req.body.genre;
+    console.log(name) 
 
+    
+    MongoClient.connect("mongodb://127.0.0.1:27017/alison", function (error, db) {
+        if (error) {
+            throw error;
+
+        } else {
+            console.log("Connecté à la base de données 'alison'");
+            var dbo = db.db("alison");
+
+           dbo.collection("personnages").insertOne({name, genre}),function (err, result) {
+               if (err) throw err;           
+         
+           
+             res.send(result);
+            
+                db.close();
+           
+            }
+    
+    }
+});
+  
+})
+  
+
+ 
 app.listen(3291, function () {
     console.log(__dirname)
     console.log('Example app listening on port 3291!')
--- myapp/public/script.js
+++ myapp/public/script.js
@@ -7,11 +7,30 @@ $.ajax({
  
 console.log(datas)
 
- 
-        $("#donnee").append( "<p class='lead'> Prénom : "+datas[0].name+" </p>" )
-        $("#donnee").append( "<p class='lead'> Genre: "+datas[0].genre+"</p>" )
-    
+ for (var  i = 0 ; i < datas.length; i++){
+
+    $("#donnee").append( "<p class='lead'> Prénom : "+datas[i].name+" </p>" )
+    $("#donnee").append( "<p class='lead'> Genre: "+datas[i].genre+"</p>" )
+
+ }  
 
    
     }
 })
+
+$(function () {
+$('#submit').click(function(){
+
+    var name = $('#prenom').val();
+    var genre = $('#genre').val();
+
+    var url='http://localhost:3291/addition';
+
+     $.post(url, { name: name, genre: genre }, function(data){
+
+  $("#rajout").append("<p class='display-3'> Addition :" + prenom + "</p>");
+   console.log(prenom)
+     });
+
+    });
+});
